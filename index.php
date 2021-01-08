fc5ceef300543840005f786ff7b8687fb00cb117
Maria Theresa N. Masapol <ma.theresa.masapol@unc.edu.ph>
2021-01-08 10:10:26 +0800

    adding for each for form, i name it as contact for and adding calling php code for my update.php and delete.php

--- index.php
+++ index.php
@@ -18,7 +18,22 @@
                 <td></td>
             </tr>
         </thead>
-      
+        <tbody>
+            <?php foreach ($contacts as $contact): ?>
+            <tr>
+                <td><?=$contact['id']?></td>
+                <td><?=$contact['name']?></td>
+                <td><?=$contact['email']?></td>
+                <td><?=$contact['phone']?></td>
+                <td><?=$contact['title']?></td>
+                <td><?=$contact['created']?></td>
+                <td class="actions">
+                    <a href="update.php?id=<?=$contact['id']?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
+                    <a href="delete.php?id=<?=$contact['id']?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
     </table>
 </body>
 </html>
