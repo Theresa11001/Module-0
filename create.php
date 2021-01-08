e93bf8ec9e56fabe3301dca95815062f01931953
Maria Theresa N. Masapol <ma.theresa.masapol@unc.edu.ph>
2021-01-08 10:36:06 +0800

    check if POST data is not empty

--- create.php
+++ create.php
@@ -2,3 +2,15 @@
 include 'functions.php';
 $pdo = pdo_connect_mysql();
 $msg = '';
+
+// Check if POST data is not empty
+if (!empty($_POST)) {
+    // Post data not empty insert a new record
+    // Set-up the variables that are going to be inserted, we must check if the POST variables exist if not we can default them to blank
+    $id = isset($_POST['id']) && !empty($_POST['id']) && $_POST['id'] != 'auto' ? $_POST['id'] : NULL;
+    // Check if POST variable "name" exists, if not default the value to blank, basically the same for all variables
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $title = isset($_POST['title']) ? $_POST['title'] : '';
+    $created = isset($_POST['created']) ? $_POST['created'] : date('Y-m-d H:i:s');
