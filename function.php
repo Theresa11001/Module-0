2bd4e597592048b5e15aec2928dd94d732724018
Maria Theresa N. Masapol <ma.theresa.masapol@unc.edu.ph>
2021-01-08 10:18:10 +0800

    adding function.php to make connection to database

---
+++ function.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>$title</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+    <nav class="navtop">
+    	<div>
+    		<h1>COVID-19 ESTABLISHMENT LOGBOOK</h1>
+            <a href="index.php"><i class="fas fa-star-o"></i>MENU</a>
+    	</div>
+    </nav>
+
+    </body>
+</html>
