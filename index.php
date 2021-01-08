29261a262d96710bf7fabf268b92bd72cc38373a
Maria Theresa N. Masapol <ma.theresa.masapol@unc.edu.ph>
2021-01-08 13:59:59 +0800

    it records from the contacts table, this will be determined by the current page the user is on, the records will be ordered by the id column,it can  easily change the order by column, for example, if I change it to created then it will sort the records by the create date instead

--- index.php
+++ index.php
@@ -6,17 +6,24 @@ $pdo = pdo_connect_mysql();
 $page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
 // Number of records to show on each page
 $records_per_page = 5;
+// Prepare the SQL statement and get records from our contacts table, LIMIT will determine the page
+$stmt = $pdo->prepare('SELECT * FROM contacts ORDER BY id LIMIT :current_page, :record_per_page');
+$stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
+$stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
+$stmt->execute();
+// Fetch the records so we can display them in our template.
+$contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+// Get the total number of contacts, this is so we can determine whether there should be a next and previous button
+$num_contacts = $pdo->query('SELECT COUNT(*) FROM contacts')->fetchColumn();
+?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>COVID-19 ESTABLISHMENT LOGBOOK</title>
-</head>
-<body>
+<?=template_header('Read')?>
 
-
-<table>
+<div class="content read">
+    <h2>◘Logbook</h2>
+    <a href="create.php" class="create-contact">Add Info</a>
+    <table>
         <thead>
             <tr>
                 <td>#</td>
@@ -45,5 +52,14 @@ $records_per_page = 5;
             <?php endforeach; ?>
         </tbody>
     </table>
-</body>
-</html>
+    <div class="pagination">
+        <?php if ($page > 1): ?>
+        <a href="read.php?page=<?=$page-1?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
+        <?php endif; ?>
+        <?php if ($page*$records_per_page < $num_contacts): ?>
+        <a href="read.php?page=<?=$page+1?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
+        <?php endif; ?>
+    </div>
+</div>
+
+<?=template_footer()?>
